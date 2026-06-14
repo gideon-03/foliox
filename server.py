1464c7dcce79f40e982ca820f0cf679911fcc90b
gideon-03 <gkandjengo@gmail.com>
2026-06-14 15:13:28 +0200

    Add server.py for Render

---
+++ server.py
@@ -0,0 +1,13 @@
+﻿import os
+import flet as ft
+from main import main
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    ft.app(
+        target=main,
+        view=ft.AppView.WEB_BROWSER,
+        host='0.0.0.0',
+        port=port,
+        assets_dir='assets',
+    )
